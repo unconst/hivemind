a0e819239ad44c2502c030616d89e5bd8e1301b3
Max Ryabinin <mryabinin0@gmail.com>
2020-05-23 13:28:51 +0300

    Move (de-)serialization to pools

--- hivemind/utils/connection.py
+++ hivemind/utils/connection.py
@@ -1,13 +1,13 @@
 from contextlib import AbstractContextManager
 from socket import socket
 from typing import Tuple
-import asyncio
+
+HEADER_SIZE = 4  # number of characters in all headers
+DESTINATION_LENGTH_SIZE = 4
+PAYLOAD_LENGTH_SIZE = 8  # number of bytes used to encode payload length
 
 
 class Connection(AbstractContextManager):
-    header_size = 4  # number of characters in all headers
-    payload_length_size = 8  # number of bytes used to encode payload length
-
     __slots__ = ('conn', 'addr')
 
     def __init__(self, conn: socket, addr: Tuple[str, int]):
@@ -20,17 +20,23 @@ class Connection(AbstractContextManager):
         sock.connect(addr)
         return Connection(sock, addr)
 
-    def send_raw(self, header: str, content: bytes):
-        self.conn.send(header.encode())
-        self.conn.send(len(content).to_bytes(self.payload_length_size, byteorder='big'))
-
+    def send_raw(self, header: str, destination: str, content: bytes):
+        assert len(header) == HEADER_SIZE
+        self.conn.sendall(header.encode())
+        self.conn.sendall(len(destination).to_bytes(DESTINATION_LENGTH_SIZE, byteorder='big'))
+        self.conn.sendall(destination.encode())
+        self.conn.sendall(len(content).to_bytes(PAYLOAD_LENGTH_SIZE, byteorder='big'))
         self.conn.sendall(content)
 
     def recv_header(self) -> str:
-        return self.conn.recv(self.header_size).decode()
+        return self.conn.recv(HEADER_SIZE).decode()
+
+    def recv_destination(self) -> str:
+        length = int.from_bytes(self.conn.recv(DESTINATION_LENGTH_SIZE), byteorder='big')
+        return self.conn.recv(length).decode()
 
     def recv_raw(self, max_package: int = 2048) -> bytes:
-        length = int.from_bytes(self.conn.recv(self.payload_length_size), byteorder='big')
+        length = int.from_bytes(self.conn.recv(PAYLOAD_LENGTH_SIZE), byteorder='big')
         chunks = []
         bytes_recd = 0
         while bytes_recd < length:
@@ -43,45 +49,11 @@ class Connection(AbstractContextManager):
         assert len(ret) == length
         return ret
 
-    def recv_message(self) -> Tuple[str, bytes]:
-        return self.recv_header(), self.recv_raw()
+    def recv_message(self) -> Tuple[str, str, bytes]:
+        return self.recv_header(), self.recv_destination(), self.recv_raw()
 
     def __exit__(self, *exc_info):
         self.conn.close()
-
-
-class AsyncConnection(Connection):
-    async def recv_message(self):
-        header = await self.recv_header()
-        message = await self.recv_raw()
-        return header, message
-
-    async def recv_header(self):
-        loop = asyncio.get_running_loop()
-        data = await loop.sock_recv(self.conn, self.header_size)
-        return data.decode()
-
-    async def recv_raw(self, max_package: int = 2048):
-        loop = asyncio.get_running_loop()
-        raw_length = await loop.sock_recv(self.conn, self.payload_length_size)
-        length = int.from_bytes(raw_length, byteorder='big')
-        chunks = []
-        bytes_recd = 0
-        while bytes_recd < length:
-            chunk = await loop.sock_recv(self.conn, min(length - bytes_recd, max_package))
-            if chunk == b'':
-                raise RuntimeError("socket connection broken")
-            chunks.append(chunk)
-            bytes_recd = bytes_recd + len(chunk)
-        ret = b''.join(chunks)
-        assert len(ret) == length
-        return ret
-
-    async def send_raw(self, header: str, content: bytes):
-        loop = asyncio.get_running_loop()
-        await loop.sock_sendall(self.conn, header.encode())
-        await loop.sock_sendall(self.conn, len(content).to_bytes(self.payload_length_size, byteorder='big'))
-        await loop.sock_sendall(self.conn, content)
 
 
 def find_open_port():
